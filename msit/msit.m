596fc36c11e4d80e29485cb9b0bcda19b8790192
chassall <chassall@gmail.com>
2019-01-15 16:19:56 -0800

    Set MSIT parameters
    Also added the "size" condition.

--- msit/msit.m
+++ msit/msit.m
@@ -1,15 +1,10 @@
 % MSIT
 % C. Hassall
 % January, 2019
-% Based on PsychoPy code by Cameron Craddock: http://opencoglabrepository.github.io/experiment_msit.html
 %
 % Outstanding issues:
-% Blocked or mixed
-% X's or 0's
-% Target size condition (big/small)
-% Fixation/rest periods
-% Practice trials
 % Rest breaks
+% Font size
 
 %% Standard pre-script code
 close all; clear all; clc; % Clear everything
@@ -17,8 +12,8 @@ rng('shuffle'); % Shuffle the random number generator
 tic;
 
 %% Run flags
-justTesting = 1;
-windowed = 1;
+justTesting = 0;
+windowed = 0;
 
 %% Define control keys
 KbName('UnifyKeyNames');
@@ -108,17 +103,92 @@ stimSize = 48; % Size for stimuli
 stimColour = [255 255 255];
 correctResponses = [1 1 2 2]; % 1:left, 2:right
 textSize = 24; % Size for instructions and block messages
+stimSizeSmall = 18;
+stimSizeLarge = 40;
 
 % Blocks, trials, trial types
 nBlocks = 8;
-trialsPerBlock = 24;
+trialsPerBlock = 15;
+fontSizes = [18 40];
 controlStim={'100','020','003'}; % Trials types 1,2,3
+controlSize = [2 1 1; 1 2 1; 1 1 2]; % 1 is small font, 2 is big font
 controlAnswers = [1 2 3];
 controlTypes = [1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1 2 3];
-interferenceStim={'221','212','331','313','112','211','332','233','131','311','232','322'}; % Trial types 1-12
-interferenceAnswers = [1 1 1 1 2 2 2 2 3 3 3 3];
+interferenceStim={'221','212','331','313','112','211','332','233','131','311','232','322','221','212','331','313','112','211','332','233','131','311','232','322'}; % Trial types 1-12
+interferencesSize = [1 0 0; 0 1 0; 0 0 1];
+interferenceAnswers = [1 1 1 1 2 2 2 2 3 3 3 3 1 1 1 1 2 2 2 2 3 3 3 3];
 interferenceTypes = [1:12 1:12];
-blockType = [1 2 1 2 1 2 1 2]; % 1 = control, 2 = interference, set order
+
+targets = [1 2 3];
+positions = [1 2 3];
+distractors = [1 2]; 
+sizes = [1 2];
+trialTypes = CombVec(targets,positions,distractors,sizes)';
+trialOrder = Shuffle(trialTypes);
+
+% Target 1
+positions1 = [2 3];
+distractors1 = [2 3];
+sizes1 = [1 2 1 2 1 2];
+trialPool1 = CombVec(positions1,distractors1,sizes1)';
+trialPool1 = Shuffle(trialPool1);
+
+% Target 2
+positions2 = [1 3];
+distractors2 = [1 3];
+sizes2 = [1 2 1 2 1 2];
+trialPool2 = CombVec(positions2,distractors2,sizes2)';
+trialPool2 = Shuffle(trialPool2);
+
+% Target 3
+positions3 = [2 3];
+distractors3 = [2 3];
+sizes3 = [1 2 1 2 1 2];
+trialPool3 = CombVec(positions3,distractors3,sizes3)';
+trialPool3 = Shuffle(trialPool3);
+
+actualTrials = [];
+oneIndex = 1;
+twoIndex = 1;
+threeIndex = 1;
+targets = [1 1 1 1 1 2 2 2 2 2 3 3 3 3 3];
+if mod(str2double(p_number),2)
+    blockType = [1 2 1 2 1 2 1 2]; % 1 = control, 2 = interference, set order
+else
+    blockType = [2 1 2 1 2 1 2 1];
+end
+
+for b = 1:nBlocks
+    switch blockType(b)
+        case 1
+            thisTargetOrder = Shuffle(targets);
+            for t = 1:trialsPerBlock
+                switch thisTargetOrder(t)
+                    case 1
+                        actualTrials(b,t,:) = [1 1 0 2]; % Target, Position, Distractor, Size
+                    case 2
+                        actualTrials(b,t,:) = [2 2 0 2];
+                    case 3
+                        actualTrials(b,t,:) = [3 3 0 2];
+                end
+            end
+        case 2
+            thisTargetOrder = Shuffle(targets);
+            for t = 1:trialsPerBlock
+                switch thisTargetOrder(t)
+                    case 1
+                        actualTrials(b,t,:) = [1 trialPool1(oneIndex,:)];
+                        oneIndex = oneIndex + 1;
+                    case 2
+                        actualTrials(b,t,:) = [2 trialPool2(twoIndex,:)];
+                        twoIndex = twoIndex + 1;
+                    case 3
+                        actualTrials(b,t,:) = [1 trialPool1(threeIndex,:)];
+                        threeIndex = threeIndex + 1;
+                end
+            end 
+    end
+end
 
 trialTypes = [];
 for b = 1:nBlocks
@@ -131,8 +201,11 @@ for b = 1:nBlocks
 end
 
 % Instructions
-instructions{1} = 'Every few seconds, a set of three numbers (1, 2, 3, or 0)\nwill appear in the center of the screen.\nOne number will always be different from the other two.\nPress the button corresponding to the identity,\nnot the position, of the differing number.\nThe values corresponding to the buttons are:\nindex finger = 1, middle finger = 2, and ring finger = 3\nAnswer as accurately and quickly as possible.';
-instructions{2} = 'Press any key to begin experiment';
+instructions{1} = 'In the following task, three numbers will be presented on the display.\n\nFor each set of numbers, one number will always differ from the other two.\n\nPress the SPACE bar to continue...';
+instructions{2} = 'Your goal is to press the key on the response box that corresponds to the number that differs.\n\nConsider the following examples...\n\nPress the SPACE bar to continue...';
+
+slide1 = imread('Slide1.bmp');
+slide2 = imread('Slide2.bmp');
 
 %% Experiment
 try
@@ -149,6 +222,11 @@ try
     verRes = rec(4);
     xmid = round(rec(3)/2);
     ymid = round(rec(4)/2);
+    digitXPositions = [xmid - stimSizeLarge xmid xmid + stimSizeLarge];
+
+    % Prepare images
+    slide1Texture = Screen('MakeTexture', win, slide1);
+    slide2Texture = Screen('MakeTexture', win, slide2);
     
     % Display instructions
     for i = 1:length(instructions)
@@ -159,6 +237,17 @@ try
         KbPressWait(-1);
     end
     Screen('Flip',win);
+    
+    Screen('DrawTexture', win, slide1Texture);
+    Screen('Flip',win);
+    KbReleaseWait(-1);
+    KbPressWait(-1);
+    
+    Screen('DrawTexture', win, slide2Texture);
+    Screen('Flip',win);
+    KbReleaseWait(-1);
+    KbPressWait(-1);
+    
     WaitSecs(3);
     
     % To be saved to disk
@@ -167,31 +256,50 @@ try
     % Block/trial loop
     for blockNum = 1:nBlocks
         
-        thisBlockType = blockType(blockNum);
-        switch thisBlockType
-            case 1
-                theseStimuli = controlStim;
-                theseAnswers = controlAnswers;
-            case 2
-                theseStimuli = interferenceStim;
-                theseAnswers = interferenceAnswers;
-        end
+         thisBlockType = blockType(blockNum);
         
         for trialNum = 1:trialsPerBlock
             
-            thisTrialType = trialTypes(blockNum,trialNum);
-            thisTrialString = theseStimuli{thisTrialType};
-            thisCorrectResponse = theseAnswers(thisTrialType);
+            thisTarget = actualTrials(blockNum,trialNum,1); 
+            thisTargetPosition = actualTrials(blockNum,trialNum,2); 
+            thisDistractor = actualTrials(blockNum,trialNum,3);
+            thisTargetSize = actualTrials(blockNum,trialNum,4);
+            thisTrialDigits = [];
+            thisTrialDigits(1:3) = thisDistractor;
+            thisTrialDigits(thisTargetPosition) = thisTarget;
+            thisCorrectResponse = thisTarget;
             
-            Screen(win,'TextFont','Arial');
-            Screen(win,'TextSize',fixationSize);
-            DrawFormattedText(win,fixationCharacter,'center','center',textColour);
-            Screen('Flip',win);
-            WaitSecs(0.5);
-            
-            Screen(win,'TextFont','Arial');
-            Screen(win,'TextSize',stimSize);
-            DrawFormattedText(win,thisTrialString,'center','center',stimColour);
+            Screen(win,'TextFont','Arial'); % ??
+            switch thisTargetSize
+                % Small target, large distractors
+                case 1
+                    for d = 1:3
+                        if d == thisTargetPosition
+                            Screen('TextSize',win, stimSizeSmall);
+                            yOffset = stimSizeSmall/2;
+                            xOffset = stimSizeSmall/2;
+                        else
+                            Screen('TextSize',win, stimSizeLarge);
+                            yOffset = stimSizeLarge/2;
+                            xOffset = stimSizeLarge/2;
+                        end
+                        Screen('DrawText', win, num2str(thisTrialDigits(d)), digitXPositions(d)-xOffset, ymid-yOffset, stimColour);
+                    end
+                % Large target, small distractors
+                case 2
+                    for d = 1:3
+                        if d == thisTargetPosition
+                            Screen('TextSize',win, stimSizeLarge);
+                            yOffset = stimSizeLarge/2;
+                            xOffset = stimSizeLarge/2;
+                        else
+                            Screen('TextSize',win, stimSizeSmall);
+                            yOffset = stimSizeSmall/2;
+                            xOffset = stimSizeSmall/2;
+                        end
+                        Screen('DrawText', win, num2str(thisTrialDigits(d)), digitXPositions(d)-xOffset, ymid-yOffset, stimColour);
+                    end 
+            end
             Screen('Flip',win);
             
             % Clear event buffer if using the response box
@@ -252,7 +360,7 @@ try
                                 responseCorrect = 0;
                             end
                         elseif responseCode == 2
-                            if thisCorrectResponse == 0
+                            if thisCorrectResponse == 2
                                 responseCorrect = 1;
                             else
                                 responseCorrect = 0;
@@ -269,7 +377,10 @@ try
                 end
             end
             
-            thisLine = [blockNum trialNum thisBlockType thisTrialType madeResponse responseCode responseTime responseCorrect];
+            Screen('Flip',win); % Blank screen
+            WaitSecs(0.25); % 250 ms between trials
+            
+            thisLine = [blockNum trialNum thisBlockType thisTarget thisTargetPosition thisDistractor thisTargetSize madeResponse responseCode responseTime responseCorrect];
             dlmwrite(filename,thisLine,'delimiter', '\t', '-append');
             participantData = [participantData; thisLine];
             
@@ -280,20 +391,7 @@ try
                 throw(ME);
             end
         end
-        
-        %         % Rest break
-        %         if blockNum < nBlocks
-        %             Screen('TextSize',win,textSize);
-        %             DrawFormattedText(win,'you will now rest for a short time before the next block begins','center','center',[255 255 255]);
-        %             Screen('Flip',win);
-        %             WaitSecs(4);
-        %             Screen('Flip',win);
-        %             WaitSecs(20);
-        %             DrawFormattedText(win,'get ready to begin','center','center',[255 255 255]);
-        %             Screen('Flip',win);
-        %             WaitSecs(4);
-        %         end
-        
+
         % Rest break
         if blockNum < nBlocks
             Screen('TextSize',win,textSize);
